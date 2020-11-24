b00b6f09874487e45ffff8d12623f8dbe4654118
Luukuton <mleinikka@gmail.com>
2020-11-24 12:47:54 +0200

    Revert "Päivitetty Gradle ja lisätty .gradle gitignoreen"
    This reverts commit c5d28c0dabb0f513bc964221ddcbd7532b10a467.

--- src/main/java/ohtu/Main.java
+++ src/main/java/ohtu/Main.java
@@ -1,12 +1,42 @@
 package ohtu;
 
-import ohtu.userinterface.UserInterface;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+ 
+ 
+public class Main extends Application {
+     
+    @Override
+    public void start(Stage ikkuna){
+        TextField vasenTeksti = new TextField();
+        vasenTeksti.setId("vasen_teksti");
+        TextField oikeaTeksti = new TextField();
+        oikeaTeksti.setId("oikea_teksti");
+        Button nappi = new Button("Kopioi");
+        nappi.setId("nappi");
+        
+        nappi.setOnAction((event) -> {
+            oikeaTeksti.setText(vasenTeksti.getText());
+        });
 
-import java.io.IOException;
+        HBox komponenttiryhma = new HBox();
+        komponenttiryhma.setSpacing(20);
+        komponenttiryhma.getChildren().addAll(vasenTeksti, nappi, oikeaTeksti);
 
-public class Main  {
-    public static void main(String[] args) throws IOException {
-        UserInterface app = new UserInterface();
-        app.commandLine();
+        Scene nakyma = new Scene(komponenttiryhma);
+
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
     }
+
+    public static void main(String[] args) {
+        launch(Main.class);
+    }
+ 
 }
