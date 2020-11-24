bc55e1744380797ad11c3c51678d7be4c9c1182e
Tuomas Tervamäki <46777008+tuomaster@users.noreply.github.com>
2020-11-24 19:17:14 +0200

    lisätty Cucumberin tarvitsemat testiluokat sekä hakemisto feature-tiedostoille

---
+++ src/test/java/ohtu/StepDefinitions.java
@@ -0,0 +1,16 @@
+package ohtu;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class StepDefinitions {
+
+    // Cucumber step definitions here
+    @Before
+    public void setUp() {
+
+    }
+
+}
