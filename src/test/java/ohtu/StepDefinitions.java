e9397fac3e3a5bf45a9f6e8a3fa69fc5c76b7cea
armijuha <juha.arminen@helsinki.fi>
2020-12-04 08:01:01 +0200

    lisää hyväksymistestaus user storylle voin hakea kirjoja kirjan nimen ja kirjoittajan perusteella

--- src/test/java/ohtu/StepDefinitions.java
+++ src/test/java/ohtu/StepDefinitions.java
@@ -47,6 +47,12 @@ public class StepDefinitions {
         inputs.put("commandYoutube", "youtube");
 
     }
+    
+    @Given("command search is selected")
+    public void commandSearchSelected() throws IOException {
+        inputs.put("commandSearch", "search");
+
+    }
 
     @When("user enters book title {string} and writer {string}")
     public void userEntersBookTitleAndWriter(String title, String writer) throws IOException, SQLException, ClassNotFoundException {
@@ -104,6 +110,25 @@ public class StepDefinitions {
 
         runApp();
     }
+    
+    @When ("user enters category {string} which do not exists")
+    public void userEntersCategoryWhichDoNotExists(String category) throws IOException, SQLException, ClassNotFoundException {
+        when(br.readLine())
+                .thenReturn(inputs.get("commandSearch"))
+                .thenReturn(category)
+                .thenReturn(inputs.get("commandExit"));
+        runApp();
+    }
+    
+    @When ("user enters category {string} and searchTerm {string}")
+    public void userEntersCategoryAndSearchTerm (String category, String searchTerm) throws IOException, SQLException, ClassNotFoundException {
+        when(br.readLine())
+                .thenReturn(inputs.get("commandSearch"))
+                .thenReturn(category)
+                .thenReturn(searchTerm)
+                .thenReturn(inputs.get("commandExit"));
+        runApp();
+    }    
 
     @Then("system will respond with {string}")
     public void systemWillRespondWith(String expectedOutput) {
