bd2377aebb60a97679679b490f10519917118b95
Matias Himanen <matias.himanen@helsinki.fi>
2020-12-07 14:48:55 +0200

    blog ja movie olion lisäys ja haku toteutettu

--- src/main/java/ohtu/DbCommands.java
+++ src/main/java/ohtu/DbCommands.java
@@ -30,6 +30,10 @@ public class DbCommands {
 
             s.execute("CREATE TABLE Youtube_links (id INTEGER PRIMARY KEY, url TEXT NOT NULL, title TEXT, description TEXT, created DATETIME)");
 
+            s.execute("CREATE TABLE Blogs (id INTEGER PRIMARY KEY, url TEXT NOT NULL, title TEXT NOT NULL, writer TEXT, date DATETIME)");
+
+            s.execute("CREATE TABLE Movies (id INTEGER PRIMARY KEY, title TEXT NOT NULL, director TEXT, year INTEGER, length INTEGER)");
+
         } catch (org.sqlite.SQLiteException e) {
             // Tables have already been created
         }
@@ -38,10 +42,12 @@ public class DbCommands {
     public void add(Object o) throws SQLException {
         if (o instanceof Book) {
             addBook((Book) o);
-        }
-
-        if (o instanceof Youtube) {
+        } else if (o instanceof Youtube) {
             addYoutube((Youtube) o);
+        } else if (o instanceof Blog) {
+            addBlog((Blog) o);
+        } else {
+            addMovie((Movie) o);
         }
 
     }
@@ -66,6 +72,30 @@ public class DbCommands {
         p.setString(2, y.getTitle());
         p.setString(3, y.getDescription());
         p.setDate(4, y.getDate());
+
+        p.executeUpdate();
+
+    }
+
+    private void addBlog(Blog b) throws SQLException {
+        PreparedStatement p = db.prepareStatement("INSERT INTO Blogs(url, title, writer, date) VALUES (?,?,?,?)");
+
+        p.setString(1, b.getUrl());
+        p.setString(2, b.getTitle());
+        p.setString(3, b.getWriter());
+        p.setDate(4, b.getDate());
+
+        p.executeUpdate();
+
+    }
+
+    private void addMovie(Movie m) throws SQLException {
+        PreparedStatement p = db.prepareStatement("INSERT INTO Movies(title, director, year, length) VALUES (?,?,?,?)");
+
+        p.setString(1, m.getTitle());
+        p.setString(2, m.getDirector());
+        p.setInt(3, m.getYear());
+        p.setInt(4, m.getLength());
 
         p.executeUpdate();
 
@@ -116,7 +146,7 @@ public class DbCommands {
     public ArrayList<Book> searchBook(String searchTerm) throws SQLException {
         ArrayList<Book> foundBooks = new ArrayList<>();
 
-        PreparedStatement p = db.prepareStatement("SELECT * FROM books WHERE name LIKE ? OR writer LIKE ?");
+        PreparedStatement p = db.prepareStatement("SELECT * FROM Books WHERE name LIKE ? OR writer LIKE ?");
         p.setString(1, '%' + searchTerm + '%');
         p.setString(2, '%' + searchTerm + '%');
         ResultSet r = p.executeQuery();
@@ -153,6 +183,46 @@ public class DbCommands {
         }
 
         return foundYoutubeLinks;
+    }
+
+    public ArrayList<Blog> searchBlog(String searchTerm) throws SQLException {
+        ArrayList<Blog> foundBlogs = new ArrayList<>();
+
+        PreparedStatement p = db.prepareStatement("SELECT * FROM Blogs WHERE url LIKE ? OR writer LIKE ?");
+        p.setString(1, '%' + searchTerm + '%');
+        p.setString(2, '%' + searchTerm + '%');
+        ResultSet r = p.executeQuery();
+
+        while (r.next()) {
+            foundBlogs.add(new Blog(
+                    r.getString("url"),
+                    r.getString("title"),
+                    r.getString("writer"),
+                    r.getDate("date")
+            ));
+        }
+
+        return foundBlogs;
+    }
+
+    public ArrayList<Movie> searchMovie(String searchTerm) throws SQLException {
+        ArrayList<Movie> foundMovies = new ArrayList<>();
+
+        PreparedStatement p = db.prepareStatement("SELECT * FROM Movies WHERE title LIKE ? OR director LIKE ?");
+        p.setString(1, '%' + searchTerm + '%');
+        p.setString(2, '%' + searchTerm + '%');
+        ResultSet r = p.executeQuery();
+
+        while (r.next()) {
+            foundMovies.add(new Movie(
+                    r.getString("title"),
+                    r.getString("director"),
+                    r.getInt("year"),
+                    r.getInt("length")
+            ));
+        }
+
+        return foundMovies;
     }
 
     public void removeTable(String name) throws SQLException {
--- src/main/java/ohtu/Main.java
+++ src/main/java/ohtu/Main.java
@@ -5,7 +5,8 @@ import ohtu.userinterface.UserInterface;
 import java.io.IOException;
 import java.sql.SQLException;
 
-public class Main  {
+public class Main {
+
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         UserInterface app = new UserInterface("jdbc:sqlite:production.db");
         app.commandLine();
